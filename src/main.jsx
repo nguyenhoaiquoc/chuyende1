21e7e32df8b8ba65f1c46874c33ce986bec1a3c2
ttgBao <baohad987@gmail.com>
2025-10-13 14:46:04 +0700

    Update Product List Item, Link page detail, 2 List Sale and newProduct

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css"
+import{createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Detail from "./compoments/Detail";
+
+const routers = createBrowserRouter([
+  
+ {
+    path: "/",
+    children: [
+      {
+        index: true,    
+        element: <App/>,
+      },
+        {
+        path: "detail",
+        element: <Detail />,
+      },
+     
+    ],
+  },
+  
+]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
+       <RouterProvider router={routers}/>
   </React.StrictMode>
 );
